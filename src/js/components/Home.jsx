6d2017edb175bce9260a930acf2fc704a081cbd6
Japmjr <chuchitoperdomo@gmail.com>
2025-10-21 22:01:06 +0000

    ejecrcicio

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
+//const [ list, setList] = useState (	)
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	return (
+		<div className="container">
+			<h1>My Todos{inputValue}</h1>
+			<ul>
+				<li>
+					<input type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={
+							(e) => {
+								if (e.key === "Enter") {
+									setTodos(todos.concat(inputValue));
+								}
+							}
+						}
+						placeholder="What do you need to do?" />
+				</li>
+				{todos.map((item, index) => (
+
+					<li>
+						{item} <i className="fa-solid fa-trash" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
+					</li>
+
+				))}
+				<li>Make the bed <i className="fa-solid fa-trash" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i></li>
+				<li>Walk the dog <i className="fa-solid fa-trash"  onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i></li>
+				<li>Pay taxes <i className="fa-solid fa-trash"  onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i></li>
+				<li>Go on vacation <i className="fa-solid fa-trash"  onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i></li>
+
+			</ul>
+			<div>23 taks</div>
 		</div>
 	);
 };
